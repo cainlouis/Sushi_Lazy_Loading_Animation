74f37b44c1a4a8a9d972b9e0ab388dbc412bf089
Nael Louis <nael.louis1002@gmail.com>
2021-09-28 21:01:15 -0400

    Write comments

--- assessment/04_assignment2/starter/index.js
+++ assessment/04_assignment2/starter/index.js
@@ -10,6 +10,7 @@ function setup() {
   getCount();
 }
 
+//fetch the json from the url in input
 async function fetchApi(url) {
   let response = await fetch(url); 
   if (response.ok) {
@@ -20,16 +21,18 @@ async function fetchApi(url) {
   }
 }
 
+//get the number of sushi for both sushi type
 async function getCount() {
   try {
+    //Construct the url to get the count
     let urlMaki = global.startUrl + "number=maki";
+    //fetch the count
     let makiCountJson = await fetchApi(urlMaki);
-    global.countMaki = makiCountJson.count; 
-    console.log(global.countMaki);
+    //assign the number of count to global variable
+    global.countMaki = makiCountJson.count;
     let urlNigiri = global.startUrl + "number=nigiri";
     let nigiriCountJson = await fetchApi(urlNigiri);
     global.countNigiri = nigiriCountJson.count;
-    console.log(global.countNigiri);
     getSection();
   } catch(err) {
     treatError(err);
@@ -37,28 +40,40 @@ async function getCount() {
 }
 
 function treatError(err) {
+  //change the visibility of the error paragraph 
   let errPara = document.querySelector("#error");
   errPara.style.visibility = "visible";
+  //write to the console the error
   console.error(err);
 }
 
 async function getSection() {
+  //select the section for maki
   let parentMaki = document.querySelector("#maki");
+  //get the card-container inside the section 
   let makiContainer = parentMaki.querySelector(".card-container");
+
   let parentNigiri = document.querySelector("#nigiri");
   let nigiriContainer = parentNigiri.querySelector(".card-container");
+  //call get content for both sushi type
   getContent(global.countMaki, "maki", makiContainer);
   getContent(global.countNigiri, "nigiri", nigiriContainer);
 }
 
+//get the content of the url built and add it to the container received as input
 async function getContent(count, value, container)
 {
+  //for the number of sushi for the section(value)
   for (let i = 0; i < count; i++) {
     try {
+      //build the url for that sushi
       let endUrl = `category=${value}&num= + ${i}`;
       let fullUrl = global.startUrl + endUrl;
+      //fetch the json from the built url
       let json = await fetchApi(fullUrl);
+      //create article from the json received
       let article = createArticle(json);
+      //append the article to the container
       container.appendChild(article);
     } catch (err) {
       treatError(err);
@@ -67,6 +82,7 @@ async function getContent(count, value, container)
 }
 
 function createArticle(json) {
+  //create all element and their attributes/class
   let article = document.createElement("article");
   article.classList.add("card");
 
@@ -88,6 +104,6 @@ function createArticle(json) {
   article.appendChild(figure);
   section.appendChild(p);
   article.appendChild(section);
-
+  //return the article
   return article;
 }
